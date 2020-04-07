8290a94172fb89694785b7d58d3ee78e2b4b5bf7
Jan Kowalewski <jkowalewski@antmicro.com>
2020-04-07 17:57:48 +0200

    tools/spi_nor_config_generator: rework to a new way of configuring chips

--- tools/spi_nor_config_generator/override_cfg.py
+++ tools/spi_nor_config_generator/override_cfg.py
@@ -1,72 +1,67 @@
-from litespi.spi_nor_features import SpiNorFeatures as Config
-
 # Some features could be not mentioned in modules we parse from.
 # In case there is a chip which supports a feature but it was not
 # mentioned, we can override config here.
+
+# Override config template:
+# 'chip_name' : {
+#   'vendor_id' : SpiNorFlashManufacturerIDs,
+#   'device_id' : int,
+#   'total_size' : int,
+#   'page_size' : int,
+#   'total_pages' : int,
+#   'dummy_bits' : int,
+#   'addr_widths' : list(int), # [1,2,4,8]
+#   'cmd_widths' : list(int), # [1,2,4,8]
+#   'ddr_support' : bool,
+#   'dual_support' : bool,
+#   'quad_support' : bool,
+#   'octal_support' : bool,
+#   'fast_read_support' : bool,
+#   'addr32_support' : bool,
+# }
+
 override_chip_cfg = {
         'mx25lm51245' : {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_OCTAL
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'octal_support' : True,
         },
         'mx25r512f' : {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'mx25r1035f' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'mx25r2035f' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'mx25r4035f' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'mx25r8035f' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'mx25r1635f' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'w25q64jv' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'gd25q512mc' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'is25lp512m' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
         'is25wp512m' :  {
-            'supported_modes' : Config.FEATURE_SINGLE
-                                | Config.FEATURE_DUAL
-                                | Config.FEATURE_QPI
-                                | Config.FEATURE_ADDR_WIDTH_1,
+            'dual_support' : True,
+            'quad_support' : True,
         },
 }
