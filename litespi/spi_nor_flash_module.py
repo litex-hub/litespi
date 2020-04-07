d0fb0ec9b0067f6cbd37c76bee347c71821eec4c
Jan Kowalewski <jkowalewski@antmicro.com>
2020-04-07 18:02:46 +0200

    litespi: rework library to a new way of configuring chips

--- litespi/spi_nor_flash_module.py
+++ litespi/spi_nor_flash_module.py
@@ -126,8 +126,21 @@ class SpiNorFlashModule(metaclass=MetaSizes):
 
     Parameters
     ----------
-    spi_nor_cfg : SpiNorConfig
-        User defined configuration for a chip.
+    default_read_cmd : SpiNorFlashOpCode
+        Default user defined read command also used to
+        configure a chip into initial desired mode.
+
+    read_cmds : list of SpiNorFlashOpCode
+        List of read commands that can be used by SPI NOR controller
+        default: default_read_cmd
+
+    program_cmd : SpiNorFlashOpCode
+        User defined program command.
+        default: PP_1_1_1 (single mode page program)
+
+    erase_cmd : SpiNorFlashOpCode
+        User defined erase command. Default value is Sector Erase.
+        default: SE (sector erase)
 
     Attributes
     ----------
@@ -149,23 +162,28 @@ class SpiNorFlashModule(metaclass=MetaSizes):
     total_pages : int
         chip's total number of pages
 
-    supported_modes : SpiNorConfig
-        supported access modes of a chip
-
     addr_width : int
         number of lines used to send address
 
     addr_bits : int
         number of address bits (24 or 32)
 
+    supported_opcodes : list of SpiNorFlashOpCode
+        list of supported opcodes (READ/PP)
+
     dummy_bits : int
         number of dummy bits to send needed for a correct transfer
+
+    fast_mode : bool
+        chip configured in fast mode
+        True -> dummy_bits used
+        False -> no dummy_bits
 
     cmd_width : int
         number of lines used to send command
 
-    read_opcode : SpiNorFlashOpCode
-        command used to read the memory
+    read_cmds : list of SpiNorFlashOpCode
+        list of available read commands
 
     program_opcode : SpiNorFlashOpCode
         command used to program the memory
@@ -178,103 +196,75 @@ class SpiNorFlashModule(metaclass=MetaSizes):
 
     ddr : bool
         setup module to run in DDR mode
+
+    Properties
+    ----------
+    read_opcode : SpiNorFlashOpCode
+        command used to read the memory
+        as well as configures chip into desired mode
     """
 
-    def _configure_transfer_mode(self, cfg):
-        """Configure Transfer Mode
+    @property
+    def read_opcode(self):
+        return self._read_opcode
 
-        Funtion ``_configure_transfer_mode`` setups fields:
+    @read_opcode.setter
+    def read_opcode(self, read_cmd):
+        """read opcode
 
-            * addr_bits
+        Property ``read_opcode`` defines chip mode,
+        hence follwing attributes are set:
+
+            * cmd_width
             * addr_width
             * bus_width
-
-        according to user defined config and checks if only one
-        mode has been selected.
-
-        Arguments
-        ---------
-        cfg : SpiNorFeatures
-            User defined flags used to configure chip into desired mode.
-
-        Return value
-        ------------
-        bool
-            True: correct chip configuration
-            False: more than one mode selected (incorrect configuration)
+            * addr_bits
+            * ddr
         """
 
-        if SpiNorFeatures.FEATURE_4BA in cfg:
+        cfg = SpiNorFlashOpCodes()
+        cfg.name = SpiNorFlashOpCodes.name_from_value(SpiNorFlashOpCodes, read_cmd)
+
+        self.ddr = "DTR" in cfg.name
+        if "4B" in cfg.name:
             self.addr_bits = 32
         else:
             self.addr_bits = 24
 
-        # If there is no addr_width config - select addr_width=1
-        if SpiNorFeatures.FEATURE_ADDR_WIDTH_1 in cfg:
-            self.addr_width = 1
-        elif SpiNorFeatures.FEATURE_ADDR_WIDTH_2 in cfg:
-            self.addr_width = 2
-        elif SpiNorFeatures.FEATURE_ADDR_WIDTH_4 in cfg:
-            self.addr_width = 4
-        elif SpiNorFeatures.FEATURE_ADDR_WIDTH_8 in cfg:
-            self.addr_width = 8
-        else:
-            self.addr_width = 1
 
-        # Only one transmission mode can be selected
-        if SpiNorFeatures.FEATURE_SINGLE in cfg:
-            self.bus_width = 1
-            return ((SpiNorFeatures.FEATURE_DUAL not in cfg) and
-                   (SpiNorFeatures.FEATURE_QPI not in cfg) and
-                   (SpiNorFeatures.FEATURE_OCTAL not in cfg))
-        elif SpiNorFeatures.FEATURE_DUAL in cfg:
-            self.bus_width = 2
-            return ((SpiNorFeatures.FEATURE_SINGLE not in cfg) and
-                   (SpiNorFeatures.FEATURE_QPI not in cfg) and
-                   (SpiNorFeatures.FEATURE_OCTAL not in cfg))
-        elif SpiNorFeatures.FEATURE_QPI in cfg:
-            self.bus_width = 4
-            return ((SpiNorFeatures.FEATURE_SINGLE not in cfg) and
-                   (SpiNorFeatures.FEATURE_DUAL not in cfg) and
-                   (SpiNorFeatures.FEATURE_OCTAL not in cfg))
-        elif SpiNorFeatures.FEATURE_OCTAL in cfg:
-            self.bus_width = 8
-            return ((SpiNorFeatures.FEATURE_SINGLE not in cfg) and
-                   (SpiNorFeatures.FEATURE_DUAL not in cfg) and
-                   (SpiNorFeatures.FEATURE_QPI not in cfg))
+        self.cmd_width = int(cfg.cmd_width)
+        self.addr_width = int(cfg.addr_width)
+        self.bus_width = int(cfg.data_width)
 
-    def _configure_chip(self, cfg, erase_cmd, program_cmd, ddr_mode):
-        if cfg not in self.supported_modes:
-            raise ValueError("Configuration (%s) not supported in chip %s!" % (cfg, self.name))
-        assert self._configure_transfer_mode(cfg)
+        self.fast_mode = "FAST" in cfg.name or self.bus_width > 1
 
-        self.cmd_width = 1 # Currently we support only cmd_width=1
-        self.ddr = ddr_mode
+        self._read_opcode = read_cmd
 
-        # Find out what read command we can use
-        addr32 = ''
-        fast = ''
-        if SpiNorFeatures.FEATURE_4BA in cfg:
-            addr32 = '_4B'
-        if SpiNorFeatures.FEATURE_SINGLE in cfg:
-            if SpiNorFeatures.FEATURE_FAST_READ in cfg:
-                fast = '_FAST'
-            else:
-                self.dummy_bits = 0
-                self.read_opcode = getattr(SpiNorFlashOpCodes, "READ%s%s" %
-                          (fast, addr32))
-        else:
-            self.read_opcode = getattr(SpiNorFlashOpCodes, "READ_%d_%d_%d%s" %
-                          (self.cmd_width, self.addr_width, self.bus_width, addr32))
+    def check_bus_width(self, width):
+        return width == self.bus_width
 
-        # Set rest of the commands
-        self.erase_opcode = erase_cmd
+    def _configure_chip(self, default_read_cmd,
+                        erase_cmd,
+                        program_cmd):
+        # Check if commands on the list are supported
+        for cmd in self.read_cmds:
+            if cmd not in self.supported_opcodes:
+                raise ValueError("""\
+Read command (%s) not supported in chip %s!""" % (str(cmd), self.name))
+
+        # Check if program command is supported
+        if program_cmd not in self.supported_opcodes:
+            raise ValueError("Read command (%s) not supported in chip %s!" % (str(read_cmd), self.name))
+
+        # Set commands
+        self.read_opcode = default_read_cmd
         self.program_opcode = program_cmd
+        self.erase_opcode = erase_cmd
 
-    def __init__(self, spi_nor_cfg=SpiNorFeatures.FEATURE_SINGLE,
-                 erase_cmd=SpiNorFlashOpCodes.SE,
-                 program_cmd=SpiNorFlashOpCodes.PP,
-                 ddr_mode=False):
+    def __init__(self, default_read_cmd,
+                 read_cmds=[],
+                 program_cmd=SpiNorFlashOpCodes.PP_1_1_1,
+                 erase_cmd=SpiNorFlashOpCodes.SE):
         # Check if mandatory attributes are set by an inheritor
         assert hasattr(self, 'manufacturer_id')
         assert hasattr(self, 'device_id')
@@ -282,11 +272,16 @@ class SpiNorFlashModule(metaclass=MetaSizes):
         assert hasattr(self, 'total_size')
         assert hasattr(self, 'page_size')
         assert hasattr(self, 'total_pages')
-        assert hasattr(self, 'supported_modes')
+        assert hasattr(self, 'supported_opcodes')
         assert hasattr(self, 'dummy_bits')
 
-        # Configure a chip using provided config
-        self._configure_chip(spi_nor_cfg, erase_cmd, program_cmd, ddr_mode)
+        # Make sure default read command is on the list
+        if default_read_cmd not in read_cmds:
+            read_cmds.append(default_read_cmd)
 
-    def check_bus_width(self, width):
-        return width == self.bus_width
+        self.read_cmds = read_cmds
+
+        # Configure a chip using provided default_read_cmd
+        self._configure_chip(default_read_cmd,
+                             erase_cmd,
+                             program_cmd)
