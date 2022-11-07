6909633007a1f8b9e086bd3601cf18c1b9892d20
Florent Kermarrec <florent@enjoy-digital.fr>
2022-11-07 17:38:53 +0100

    litespi_gen: Rewrite a simpler version just using command line parameters.
    Since the generator is simple enough, it's not really useful to use .yml files and
    we can configure it directly from command line parameters as done on LiteSDCard
    generator.
    
    Also improve separation of SPI Flash module, PHY, Core and MMAP interface to ease
    future maintenance/additional features.
    
    Example of use:
    litespi_gen --bus-standard=axi-lite --module=MT25QL256 --mode=x1 --rate=1:1
    litespi_gen --bus-standard=wishbone --module=S25FL256L --mode=x4 --rate=1:1
    
    Add --sim to integrate the SPIFlash model.

--- litespi/gen.py
+++ litespi/gen.py
@@ -22,14 +22,18 @@ easily a custom configuration of the core.
 
 """
 
-import argparse
 import os
 import yaml
+import argparse
+import inspect
 
 from migen import *
 
 from litex.build.generic_platform import *
+from litex.build.sim import SimPlatform
 from litex.build.xilinx.platform import XilinxPlatform
+from litex.build.altera.platform import AlteraPlatform
+from litex.build.lattice.platform import LatticePlatform
 
 from litex.soc.interconnect import wishbone
 from litex.soc.interconnect import axi
@@ -37,21 +41,19 @@ from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.soc.integration.soc import SoCRegion
 
-import inspect
 import litespi.modules
 from litespi.spi_nor_flash_module import SpiNorFlashModule
+from litespi.opcodes import SpiNorFlashOpCodes as Codes
 
-# module database
-module_classes = inspect.getmembers(litespi.modules, inspect.isclass)
-devices_dict = {name: cls for name, cls in module_classes if issubclass(cls, SpiNorFlashModule)}
-
+from litespi import LiteSPI
+from litespi.phy.generic import LiteSPIPHY
 
 # IOs ----------------------------------------------------------------------------------------------
 
 _io = [
     # Clk / Rst
-    ("sys_clock", 0, Pins(1)),
-    ("sys_reset", 1, Pins(1)),
+    ("clk", 0, Pins(1)),
+    ("rst", 1, Pins(1)),
 
     # SPI-Flash (X1).
     ("spiflash", 0,
@@ -71,128 +73,126 @@ _io = [
     ),
 ]
 
-class SPICore(SoCMini):
-    def __init__(self, platform, core_config):
+# LiteSPI Core -------------------------------------------------------------------------------------
 
-        # SoC parameters ---------------------------------------------------------------------------
-        soc_args = {}
-        if "soc" in core_config:
-            soc_config = core_config["soc"]
-
-            for arg in soc_config:
-                if arg in ("csr_map", "interrupt_map", "mem_map"):
-                    getattr(self, arg).update(soc_config[arg])
-                else:
-                    soc_args[arg] = soc_config[arg]
+class LiteSPICore(SoCMini):
+    def __init__(self, platform, module, mode="x4", rate="1:1", divisor="1", bus_standard="wishbone", bus_endianness="big", sim=False):
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = CRG(platform.request("clk"), platform.request("rst"))
 
         # SoCMini ----------------------------------------------------------------------------------
-        SoCMini.__init__(self, platform, clk_freq=int(1e6), **soc_args)
+        SoCMini.__init__(self, platform, clk_freq=int(1e6))
 
-        # CRG --------------------------------------------------------------------------------------
-        self.submodules.crg = CRG(platform.request("sys_clock"), platform.request("sys_reset"))
+        # SPI Flash Module -------------------------------------------------------------------------
 
-        device = core_config["device"]
+        # Get available modules.
+        modules_cls = inspect.getmembers(litespi.modules, inspect.isclass)
+        modules     = {name: cls for name, cls in modules_cls if issubclass(cls, SpiNorFlashModule)}
 
-        bus_standard = core_config["bus_standard"]
-        assert bus_standard in ["wishbone", "axi-lite", "sim"]
+        # Check that selected module is supported.
+        def print_supported_modules():
+            r = ""
+            i = 0
+            modules_per_line = 8
+            modules_interval = 24
+            for name, module in modules.items():
+                r += f"{name}{' '*(modules_interval-len(name))}"
+                if (i%modules_per_line) == (modules_per_line-1):
+                    r += "\n"
+                i += 1
+            return r
 
-        mode = core_config["mode"]
-        if not device in devices_dict:
-            raise ValueError(f"Unsupported SPI device:", device)
+        if not module in modules:
+            raise ValueError(f"Unsupported SPI module {module}, supported:\n{print_supported_modules()}")
 
+        # Check that selected width is supported in module.
         assert mode in ["x1", "x4"]
-        requested_bus_width = {
+        module_width = {
           "x1" : 1,
           "x4" : 4,
-          }[mode]
-        
-        rate = core_config["rate"] if "rate" in core_config else "1:1"
-        divisor = int(core_config["divisor"]) if "divisor" in core_config else 1
-        
-        from litespi.opcodes import SpiNorFlashOpCodes as Codes
-        module_class = devices_dict[device]
-        opcode = module_class.supported_opcodes[0] #use any opcode to get the instance
-        spiflash_module = module_class(opcode)
-        if not spiflash_module.check_bus_width(width=requested_bus_width):
-            raise ValueError(f"SPI device doesn't support {requested_bus_width}-bit bus with")
+        }[mode]
+        spiflash_module_cls = modules[module]
+        spiflash_module     = spiflash_module_cls(getattr(Codes, f"READ_1_1_{module_width}"))
 
-        pads = self.platform.request("spiflash" if mode == "x1" else "spiflash4x")
+        # SPI Flash PHY ----------------------------------------------------------------------------
 
-        # PHY --------------------------------------------------------------------------------------
-        if "phy" in core_config:
-            assert core_config["phy"] == "sim"
+        if sim:
             from litespi.phy.model import LiteSPIPHYModel
-            from litex.soc.integration.common import get_mem_data
-            mem_init = get_mem_data("spiflash.bin") #generates "litespi_core_mem.init" file
-            spiflash_phy = LiteSPIPHYModel(spiflash_module, init=mem_init)
-        else:            
-            from litespi.phy.generic import LiteSPIPHY
-            spiflash_phy = LiteSPIPHY(pads, spiflash_module, device=self.platform.device,
-                default_divisor=divisor, rate=rate)
+            spiflash_phy = LiteSPIPHYModel(spiflash_module, init=[i for i in range(16)]) # FIXME: Allow custom init?
+            self.submodules += spiflash_phy
+        else:
+            pads = self.platform.request("spiflash" if mode == "x1" else "spiflash4x")
+            spiflash_phy = LiteSPIPHY(
+                pads            = pads,
+                flash           = spiflash_module,
+                device          = platform.device,
+                default_divisor = int(divisor),
+                rate            = rate
+            )
+            self.submodules += spiflash_phy
 
-        self.submodules += spiflash_phy
 
-        from litespi import LiteSPI
-        spiflash_core = LiteSPI(spiflash_phy, mmap_endianness=self.cpu.endianness,
-          with_master=False, with_mmap=True, with_csr=False) #FIXME: parametrize endianness
+        # SPI Flash Core / MMAP --------------------------------------------------------------------
+
+        assert bus_standard in ["wishbone", "axi-lite"]
+
+        spiflash_core = LiteSPI(
+            phy             = spiflash_phy,
+            mmap_endianness = bus_endianness,
+            with_master     = False,
+            with_mmap       = True,
+            with_csr        = False
+        )
         self.submodules += spiflash_core
-        
 
         # Wishbone.
         if bus_standard == "wishbone":
+            # LiteSPI is already in Wishbone, just expose the Bus.
             platform.add_extension(spiflash_core.bus.get_ios("bus"))
-            platform_bus = self.platform.request("bus")
-            self.comb += spiflash_core.bus.connect_to_pads(platform_bus, mode="slave")
+            self.comb += spiflash_core.bus.connect_to_pads(platform.request("bus"), mode="slave")
 
         # AXI-Lite.
         if bus_standard == "axi-lite":
             # LiteSPI is in Wishbone, converter to AXI-Lite and expose the AXI-Lite Bus.
             axil_bus = axi.AXILiteInterface(address_width=32, data_width=32)
             platform.add_extension(axil_bus.get_ios("bus"))
-
-            #adapter should be Wishbone2AXILite the opposite for masters
             self.submodules += axi.AXILite2Wishbone(axil_bus, spiflash_core.bus)
-            self.comb += axil_bus.connect_to_pads(self.platform.request("bus"), mode="slave")
+            self.comb += axil_bus.connect_to_pads(platform.request("bus"), mode="slave")
 
 # Build --------------------------------------------------------------------------------------------
 
 def main():
-    parser = argparse.ArgumentParser(description="LiteSPI standalone core generator")
-    builder_args(parser)
-    parser.set_defaults(output_dir="build")
-    parser.add_argument("--vendor",       default="xilinx",  help="FPGA Vendor.")
-    parser.add_argument("--device",       default="",  help="FPGA device (required for 1:2 rate)")
-    parser.add_argument("config", help="YAML config file")
+    parser = argparse.ArgumentParser(description="LiteSPI standalone core generator.")
+    parser.add_argument("--clk-freq",       default="100e6",      help="Input Clk Frequency.")
+    parser.add_argument("--vendor",         default="xilinx",     help="FPGA Vendor.")
+    parser.add_argument("--module",         default="S25FL128L",  help="SPI Flash Module.")
+    parser.add_argument("--mode",           default="x1",         help="SPI Mode (x1, x4).")
+    parser.add_argument("--rate",           default="1:1",        help="SPI Flash Core rate (1:1, 1:2).")
+    parser.add_argument("--divisor",        default="1",          help="SPI PHY Clk Divisor.")
+    parser.add_argument("--bus-standard",   default="wishbone",   help="Bus Standard (wishbone, axi-lite).")
+    parser.add_argument("--bus-endianness", default="big",        help="Bus Endianness (big, little).")
+    parser.add_argument("--sim",             action='store_true', help="Integrate LiteSPIPHYModel in core for simulation.")
     args = parser.parse_args()
-    core_config = yaml.load(open(args.config).read(), Loader=yaml.Loader)
-
-    # Convert YAML elements to Python/LiteX --------------------------------------------------------
-    for k, v in core_config.items():
-        replaces = {"False": False, "True": True, "None": None}
-        for r in replaces.keys():
-            if v == r:
-                core_config[k] = replaces[r]
 
     # Generate core --------------------------------------------------------------------------------
-    # Convert/Check Arguments ----------------------------------------------------------------------
     platform_cls = {
         "xilinx"  : XilinxPlatform,
+        "altera"  : AlteraPlatform,
+        "intel"   : AlteraPlatform,
+        "lattice" : LatticePlatform
     }[args.vendor]
-    platform = platform_cls(device=args.device, io=_io)
-    platform.add_extension(_io)
-
-    if core_config["bus_standard"] in ["wishbone", "axi-lite", "sim"]:
-        soc = SPICore(platform, core_config)
-    else:
-        raise ValueError("Unknown bus standard: {}".format(core_config["bus_standard"]))
-
-    builder_arguments = builder_argdict(args)
-    builder_arguments["compile_gateware"] = False
-    if builder_arguments["csr_csv"] is None:
-        builder_arguments["csr_csv"] = os.path.join(builder_arguments["output_dir"], "csr.csv")
-
-    builder = Builder(soc, **builder_arguments)
-    builder.build(build_name="litespi_core")
+    platform = platform_cls(device="", io=_io)
+    core     = LiteSPICore(platform,
+        module         = args.module,
+        mode           = args.mode,
+        rate           = args.rate,
+        divisor        = args.divisor,
+        bus_standard   = args.bus_standard,
+        bus_endianness = args.bus_endianness,
+        sim            = args.sim,
+    )
+    builder  = Builder(core, output_dir="build")
+    builder.build(build_name="litespi_core", run=False)
 
 if __name__ == "__main__":
     main()
