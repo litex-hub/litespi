09d42f13b66631a44eb6162edba46dc5c11242cf
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 10:06:23 +0100

    setup.py: Improve indentation.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from setuptools import find_packages
 
 
 setup(
-    name="litespi",
-    description="Small footprint and configurable SPI core",
-    author="LiteSPI Developers",
-    url="https://github.com/litex-hub",
-    download_url="https://github.com/litex-hub/litespi",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
-    entry_points={
+    name                 = "litespi",
+    description          = "Small footprint and configurable SPI core",
+    author               = "LiteSPI Developers",
+    url                  = "https://github.com/litex-hub",
+    download_url         = "https://github.com/litex-hub/litespi",
+    test_suite           = "test",
+    license              = "BSD",
+    python_requires      = "~=3.6",
+    packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data = True,
+    entry_points         = {
         "console_scripts": [
             "litespi_gen=litespi.gen:main",
         ],
