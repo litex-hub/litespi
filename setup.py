9d77a186bb275707eb53b98136bbfc5a5a8e9459
Florent Kermarrec <florent@enjoy-digital.fr>
2025-01-07 09:25:31 +0100

    setup.py: Update for consistency with other setup.py scripts.

--- setup.py
+++ setup.py
@@ -4,19 +4,35 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+
 setup(
-    name                 = "litespi",
-    description          = "Small footprint and configurable SPI core",
-    author               = "LiteSPI Developers",
-    url                  = "https://github.com/litex-hub",
-    download_url         = "https://github.com/litex-hub/litespi",
-    test_suite           = "test",
-    license              = "BSD",
-    python_requires      = "~=3.7",
-    packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data = True,
-    keywords             = "HDL ASIC FPGA hardware design",
-    classifiers          = [
+    name                          = "litespi",
+    version                       = "2024.08",
+    description                   = "Small footprint and configurable SPI core",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    author                        = "LiteSPI Developers",
+    url                           = "https://github.com/litex-hub",
+    download_url                  = "https://github.com/litex-hub/litespi",
+    test_suite                    = "test",
+    license                       = "BSD",
+    python_requires               = "~=3.7",
+    install_requires              = ["pyyaml", "litex"],
+    extras_require                = {
+        "develop": [
+          "meson"
+          "pexpect"
+          "setuptools"
+          "requests"
+        ]
+    },
+    packages                      = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data          = True,
+    keywords                      = "HDL ASIC FPGA hardware design",
+    classifiers                   = [
         "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
         "Environment :: Console",
         "Development Status :: 3 - Alpha",
@@ -25,7 +41,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
-    entry_points         = {
+    entry_points = {
         "console_scripts": [
             "litespi_gen=litespi.gen:main",
         ],
