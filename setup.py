0de83953e3c2c697593ea54f2c81de1a217abdd2
Florent Kermarrec <florent@enjoy-digital.fr>
2025-01-07 09:40:13 +0100

    setup.py: Bump to 2024.12.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name                          = "litespi",
-    version                       = "2024.08",
+    version                       = "2024.12",
     description                   = "Small footprint and configurable SPI core",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
